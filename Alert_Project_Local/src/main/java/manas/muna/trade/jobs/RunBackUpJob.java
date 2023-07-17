bdaea5892cd1b84d6c6eaab162e91e4cb587b6cf
kumarmuna <kumarmuna@hotmail.com>
2023-07-17 20:38:04 +0530

    "feature/fix-bstockemaload |added ConditionalJob"

--- Alert_Project_Local/src/main/java/manas/muna/trade/jobs/RunBackUpJob.java
+++ Alert_Project_Local/src/main/java/manas/muna/trade/jobs/RunBackUpJob.java
@@ -1,20 +1,23 @@
 package manas.muna.trade.jobs;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RunBackUpJob {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         backupEmaData();
     }
 
-    public static void backupEmaData() {
+    public static void backupEmaData() throws Exception{
+        writeDateToFile("D:\\share-market\\GIT-PUSH\\Alert_Project_Local\\src\\main\\resources\\back-up-data\\backup_run_date.txt");
         String readFileLocation = "D:\\share-market\\GIT-PUSH\\Alert_Project_Local\\src\\main\\resources\\history_ema_data";
         String storeFileLocation = "D:\\share-market\\GIT-PUSH\\Alert_Project_Local\\src\\main\\resources\\back-up-data\\calculated-system-data";
         String storeFileSubLocation = "";
@@ -24,28 +27,32 @@ public class RunBackUpJob {
             for (String file : files){
                 storeFileSubLocation = getSubLocation(file);
                 String destinationStoreFileLocation = storeFileLocation+"\\"+storeFileSubLocation;
-                String fileDataInString = "";
-                FileReader fr = new FileReader(readFileLocation+"\\"+file);
-                BufferedReader br = new BufferedReader(fr);
-                int data;
-                while((data = br.read())!=-1){
-                    fileDataInString = fileDataInString+ data;
+                FileReader filereader = new FileReader(readFileLocation+"\\"+file);
+                CSVReader csvReader = new CSVReaderBuilder(filereader).build();
+                List<String[]> allData = csvReader.readAll();
+
+                File fl = new File(destinationStoreFileLocation+"\\"+file);
+                if (!fl.exists()){
+                    fl.createNewFile();
                 }
-                FileWriter fw = new FileWriter(destinationStoreFileLocation+"\\"+file);
-                BufferedWriter bw = new BufferedWriter(fw);
-                for(String str: fileDataInString.split(" ")){
-                    bw.write(str);
-                    bw.write(" ");
+                FileWriter outputfile = new FileWriter(destinationStoreFileLocation+"\\"+file, false);
+                CSVWriter writer = new CSVWriter(outputfile);
+                for (String[] dt : allData){
+                    writer.writeNext(dt);
                 }
-                bw.flush();
-                fr.close();
-                fw.close();
-                br.close();
-                bw.close();
+                writer.close();
             }
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private static void writeDateToFile(String path) throws Exception{
+        FileWriter fw = new FileWriter(path);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("Backup Job Run Date :"+new Date());
+        bw.flush();
+        bw.close();
     }
 
     private static String getSubLocation(String file) {
--- Alert_Project_Local/src/main/java/manas/muna/trade/jobs/TestRunTradeTask.java
+++ Alert_Project_Local/src/main/java/manas/muna/trade/jobs/TestRunTradeTask.java
@@ -10,19 +10,19 @@ import java.util.Set;
 public class TestRunTradeTask {
     public static void main(String[] args){
         //Read History Data
-//        try {
-//            StoreStockHistoryToCvsJob.execute();
-//        }catch (Exception e){
-//            System.out.println("Error during history data read");
-//        }
+        try {
+            StoreStockHistoryToCvsJob.testexecute();
+        }catch (Exception e){
+            System.out.println("Error during history data read");
+        }
 
 //        Read Excel And Calculate EMA
-        try {
-            Thread.sleep(12);
-            ReadingExcelAndCalculateEMAJob.testexecute();
-        }catch (Exception e){
-            System.out.println("Error calculate EMA");
-        }
+//        try {
+//            Thread.sleep(12);
+//            ReadingExcelAndCalculateEMAJob.testexecute();
+//        }catch (Exception e){
+//            System.out.println("Error calculate EMA");
+//        }
 
         //Send Green Notification
 //        try {
